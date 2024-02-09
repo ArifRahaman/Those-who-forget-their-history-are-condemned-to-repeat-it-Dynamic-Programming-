3fca811c5dff73c3ac554aa2b4ba7c7eb44fa3c0
Arif Rahaman <115584161+ArifRahaman@users.noreply.github.com>
2024-02-09 21:21:29 +0530

    Update 1143. Longest Common Subsequence.java

--- 1143. Longest Common Subsequence.java
+++ 1143. Longest Common Subsequence.java
@@ -26,3 +26,60 @@ class Solution {
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+//If I want to print the substring
+
+
+public class Main {
+    public static String longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        
+        int[][] dp = new int[m + 1][n + 1];
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        // Reconstruct LCS from dp array
+        int i = m, j = n;
+        StringBuilder sb = new StringBuilder();
+        while (i > 0 && j > 0) {
+            if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                sb.append(text1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+        
+        // Reverse the result because we built it backwards
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String text1 = "abcde";
+        String text2 = "ace";
+        String lcs = longestCommonSubsequence(text1, text2);
+        System.out.println("Longest Common Subsequence: " + lcs);
+    }
+}
+
